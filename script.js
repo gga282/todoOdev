d0d5b064b7e31136c7014dcde314dde3b6033d18
gga282 <103218918+gga282@users.noreply.github.com>
2022-07-17 19:24:55 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,78 +1,80 @@
-const form=document.querySelector('#todoAppForm');
-const input=document.querySelector('#inputText');
-const btnDeleteAll=document.querySelector('#btnDeleteAll');
 const todoDone=document.querySelector('#todoDone');
-const todoInProgress=document.querySelector('#todoInProgress');
+const todoProgress=document.querySelector('#todoProgress');
 const todoPendings=document.querySelector('#todoPendings');
-const x=todoInProgress.addEventListener("mouseover", myDrop);
-
-var holdAndDrag;
-var count=0;
+const deleteAllButton=document.querySelector('#deleteAllButton');
+const inputText=document.querySelector('#inputText');
+const submitTodo=document.querySelector('#submitTodo');
+console.log("hello");
+console.log(inputText.value);
+var i=0;
+let items;
 
 addAllEventListener();
 
 function addAllEventListener(){
-    form.addEventListener('submit',addNewItem);
-    todoPendings.addEventListener('click',deleteItem);
-    todoInProgress.addEventListener('click',deleteItem);
-    todoDone.addEventListener('click',deleteItem);
-    btnDeleteAll.addEventListener('click',removeAll);
+    console.log("hello2");
+    
+
+todoDone.addEventListener('click',deleteItem);
+todoPendings.addEventListener('click',deleteItem);
+todoProgress.addEventListener('click',deleteItem);
+deleteAllButton.addEventListener('click',removeAll);
+submitTodo.addEventListener('submit',addNewItem);
 
 }
-function removeAll(e){
-    todoInProgress.innerHTML="";
-    todoDone.innerHTML="";
-    todoPendings.innerHTML="";
+function removeAll(){
+    console.log('deneme');
+    todoDone.innerHTML=" ";
+    todoProgress.innerHTML=" ";
+    todoPendings.innerHTML=" ";
 }
 function addNewItem(e){
-    e.preventDefault();
 
-    if(input.value===''){
-        alert('Please type a to do');
+    if(inputText.value!=''){
+        createItems(inputText.value);
+    }
+    else{
+        alert("Please enter value");
         return;
-    }else{
-        createItem(input.value);
     }
+    setItemsToLS(inputText.value);
 }
-function createItem(text){
-    var arr=[];
-    var kosul=true;
-    count++;
-    let i=count;
+function createItems(text){
+    
+    
     const li=document.createElement('li');
-    while(kosul){
-    li.setAttribute('draggable','true');
-    li.setAttribute("ondragstart","drag(event)");
-    li.setAttribute('id',''+i);
-    i++
+    li.classList="list-group list-group-flush";
     li.appendChild(document.createTextNode(text));
-    kosul=false;
-    }
-
-    /*const li=document.createElement('li');
     li.setAttribute('draggable','true');
-    li.setAttribute("ondragstart","drag(event)");
-    li.setAttribute('id','',count);
-    li.appendChild(document.createTextNode(text));
-*/
+    li.setAttribute("id",''+i);
+    i+=1;
 
-    const a = document.createElement('a');
-    a.classList = 'delete-item float-end';
-    a.setAttribute('href', '#');
-    a.innerHTML = '<i class="bi bi-x-circle-fill"></i>';
+    const a=document.createElement('a');
+    a.classList='delete-item float-end';
+    a.setAttribute('href','#');
+    a.innerHTML = '<i class="bi bi-x-circle"></i>';
 
     li.appendChild(a);
     todoPendings.appendChild(li);
-
+    
 
 }
 function deleteItem(e){
     e.preventDefault();
-    if (e.target.className == 'bi bi-x-circle-fill') {
-        if(confirm('Are you sure?')) {
-            e.target.parentElement.parentElement.remove();   
+    console.log('deneme');
+    if(e.target.className=="bi bi-x-circle"){
+        if(confirm('Are you sure?')){
+            e.target.parentElement.parentElement.remove();
         }
     }
+
+}
+function myDrop(e){
+    e.preventDefault();
+    //console.log("Birak: ",e.target.id);
+    var data=e.dataTransfer.getData("text");
+    console.log(data);
+    e.target.appendChild(document.getElementById(data));
 
 }
 function allowDrop(e){
@@ -81,14 +83,37 @@ function allowDrop(e){
 function drag(e){
     console.log("surukle:",e.target.id);
     e.dataTransfer.setData("text",e.target.id);
-    holdAndDrag=e.target;
     
 }
+function setItemsToLS(text){
+    items=getItemsFromLS();
+    items.push(text);
+    localStorage.setItem("todo-list", JSON.stringify(items));
 
-function myDrop(e){
-    e.preventDefault();
-    console.log("birak :",e.target);
-    var data=e.dataTransfer.getData("text");
-    console.log(data);
-    e.target.appendChild(document.getElementById(data));
+}
+function getItemsFromLS(){
+
+    if(localStorage.getItem("todo-list")=== null){
+        items=[];
+    }else{
+        items = JSON.parse(localStorage.getItem("todo-list"));
+    }
+    return items;
+}
+function loadItems() {
+
+    items = getItemsFromLS();
+    items.forEach(function (item) {
+        createItems(item);
+    })
+
+}
+function deleteItemFromLS(text) {
+    items = getItemsFromLS();
+    items.forEach(function (item, index) {
+        if (text == item) {
+            items.splice(index, 1);
+        }
+    });
+    localStorage.setItem("todo-list", JSON.stringify(items));
 }
